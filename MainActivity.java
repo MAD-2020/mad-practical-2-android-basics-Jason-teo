dc33c248e1042bdf50a7442fd49bf1c8d70e431e
Jason-teo <64177231+Jason-teo@users.noreply.github.com>
2020-05-04 18:59:18 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,38 +1,122 @@
 package sg.edu.np.WhackAMole;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import android.util.Log;
-
-
+import android.view.View;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* Hint
-        - The function setNewMole() uses the Random class to generate a random value ranged from 0 to 2.
-        - Feel free to modify the function to suit your program.
-    */
+    //Declare Variables
+    private static final String TAG = "Whack-A-Mole";
+
+    private TextView resultTextView;
+    private Button Button1;
+    private Button Button2;
+    private Button Button3;
+    private List<Button> buttonList = new ArrayList<>();
+    private Integer randomLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v(TAG, "Finished Pre-Initialisation!");
+        resultTextView = (TextView)findViewById(R.id.resultTextView);
+        Button1 = (Button)findViewById(R.id.button1);//using id of the buttons in xml
+        buttonList.add(Button1);
+        Button2 = (Button)findViewById(R.id.button2);
+        buttonList.add(Button2);
+        Button3 = (Button)findViewById(R.id.button3);
+        buttonList.add(Button3);
+        Log.v(TAG, "Finished Pre-initialisation.");//just to inform user
     }
 
+    //Loaded
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
-        setNewMole();
-        Log.v(TAG, "Starting GUI!");
-    }
 
+        setNewMole();
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button buttonPressed = (Button) v;
+                Log.v(TAG,"Reached");
+                switch (buttonList.indexOf(buttonPressed)) {
+                    case 0:
+                        Log.v(TAG,"Button Left Clicked!");
+                        break;
+                    case 1:
+                        Log.v(TAG,"Button Middle Clicked!");
+                        break;
+                    case 2:
+                        Log.v(TAG,"Button Right Clicked!");
+                        break;
+                    default:
+                        Log.v(TAG,"Unknown Button pressed. Found within ButtonList");
+                }
+
+                Integer score = Integer.parseInt(resultTextView.getText().toString());
+                switch (buttonPressed.getText().toString()) {
+                    case "0":
+                        Log.v(TAG,"Missed, score deducted!");
+                        score -= 1;//deduct 1 from score
+                        resultTextView.setText(score.toString());
+                        break;
+                    case "*":
+                        Log.v(TAG,"Hit, score added!");
+                        score += 1;//Add 1 to score
+                        resultTextView.setText(score.toString());
+                        break;
+                    default:
+                        Log.v(TAG,"Unknown button pressed, no case for it's text set.");
+                }
+                resetMole();
+                setNewMole();
+            }
+        };
+        Button1.setOnClickListener(listener);
+        Button2.setOnClickListener(listener);
+        Button3.setOnClickListener(listener);
+
+        Log.v(TAG,"Starting Gui.");
+    }
 
     public void setNewMole()
     {
         Random ran = new Random();
-        int randomLocation = ran.nextInt(3);
+        randomLocation = ran.nextInt(3);
+        buttonList.get(randomLocation).setText("*");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG,"Resuming.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG,"Pausing.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG,"Destroying.");
+    }
+
+    public void resetMole()
+    {
+        buttonList.get(randomLocation).setText("0");
     }
 }
+
